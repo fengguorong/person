275cb34495a68482e5d93e6882a3e1f2369bdba1
18135834685 <836599601@qq.com>
2017-01-15 14:11:38 +0800

    aadd

---
+++ personalweb/js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+// 大屏导航
+//     $(".sort").click(function(){
+//         $(".menu").slideToggle(200);
+//     })
+
+
+//导航位置
+//     var navs=document.querySelector(".navs");
+//     document.documentElement.scrollTop=1;
+//     var obj=document.documentElement.scrollTop?document.documentElement:document.body;
+//     window.onscroll=function() {
+//         if (obj.scrollTop >=680) {
+//             navs.style.cssText="position:fixed;top:0;left:0; display: block;width:100%;height:15%;background: #202428;margin: auto";
+//         } else if(obj.scrollTop<680){
+//             navs.style.cssText="position:none;display: block;width:100%;height:15%;background: #202428;margin: auto";
+//         }
+//     }
+
+
+//第二屏
+
+})
+
+
